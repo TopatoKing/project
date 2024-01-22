6539a9bff2930ba99857e20845872e5e6ebd724d
Henry Smith <smithhenry893@gmail.com>
2024-01-22 18:32:23 +0000

    turn off react strip mode

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
