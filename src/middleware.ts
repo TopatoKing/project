dddb9aedabd03fd4c917ab1ee56e5a77dcf493f5
Henry Smith <smithhenry893@gmail.com>
2024-04-19 13:04:13 +0100

    exclude with clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 /* This export allows the application to be public so the 
 user does not need to be logged in to view the site */
 export default authMiddleware({
-  publicRoutes: ["/", "/api/createUser", "/sign_in", "/contact", "/products"],
+  publicRoutes: ["/", "/api/createUser", "/sign_in", "/contact", "/products", "/api/clerk/created", "/api/clerk/updated", "/api/clerk/deleted"],
 });
 
 /* This is a route matcher for Next.js which include API routes 
